e76a4cf6357bfcfd78387f0004a24ed8b1809eb1
Joãozin <joaoandresantana38@gmail.com>
2024-10-28 15:02:32 -0300

    enfim né

---
+++ SIstema_teste01/src/Filme.java
@@ -0,0 +1,8 @@
+public class Filme {
+    String filme;
+
+
+    public Filme(String filme){
+        this.filme = filme;
+    }
+}
---
+++ SIstema_teste01/src/Sala.java
@@ -0,0 +1,9 @@
+public class Sala {
+    String codigo="01";
+    Filme filme1 = new Filme("Batman");
+
+    public Sala(String codigo, Filme filme1){
+        this.codigo= codigo;
+        this.filme1=filme1;
+    }
+}
